3448b3f973f8b36a8bb7f53048ba8c3ea38ccdfe
Aldo Colorado <62388981+AldoColorado@users.noreply.github.com>
2022-05-19 14:18:32 -0500

    CRUD's Plaza, periodos

---
+++ SourceCode/MyERP/Clases/Periodo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plataforma.Clases
+{
+    public class Periodo
+    {
+
+        public int IdPeriodo;
+        public string Nombre;
+
+        public int Activo;
+        public string ActivoStr;
+
+
+        public string Accion;
+
+    }
+}
---
+++ SourceCode/MyERP/Clases/Plaza.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plataforma.Clases
+{
+    public class Plaza
+    {
+
+        public int IdPlaza;
+        public string Nombre;
+
+        public int Activo;
+        public string ActivoStr;
+
+
+        public string Accion;
+
+    }
+}

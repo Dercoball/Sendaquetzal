0517749e85ace3c88d5459eab3767b387a1e4471
Aldo Colorado <62388981+AldoColorado@users.noreply.github.com>
2022-08-07 18:21:36 -0500

    Material de entrega

---
+++ SourceCode/plataformawebpagos/MyERP/Clases/MaterialEntrega.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plataforma.Clases
+{
+    public class MaterialEntrega
+    {
+
+        public int IdMaterialEntrega;
+        public string MaterialEntregado;
+        public float Cantidad;
+        public string Fecha;
+        public float Costo;
+        public int Eliminado;
+        public int IdEmpleado;
+        public int IdCategoria;
+
+        public Categoria Categoria;
+        public Empleado Empleado;
+
+        public string Accion;
+
+
+
+    }
+}
